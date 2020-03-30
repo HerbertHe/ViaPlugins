04e43b42803f602661e8cca0c33d045a551a7c3c
HerbertHe <Herbert.He0229@gmail.com>
2020-03-31 00:53:03 +0800

    参数调整

--- plugins/quarkLike.js
+++ plugins/quarkLike.js
@@ -26,11 +26,11 @@
     var herbSearchBox = document.createElement("div");
     herbSearchBox.id = "herb-search-box";
     herbSearchBox.style =
-        "width: 100vw; height:40px; position: fixed; left: 0; bottom: 0; display: flex; flex-direction: row; justify-content: space-around; align-items: center; z-index: 10000; background-color: white; padding: 5px; border-top: 1px solid #a5b1c2;"
+        "width: 100vw; height:40px; position: fixed; left: 0; bottom: 0; display: flex; flex-direction: row; justify-content: center; align-items: center; z-index: 10000; background-color: white; padding: 5px; border-top: 1px solid #eeeeee;"
     /* 节点扩充 */
     herbSearchBox.innerHTML =
-        '<input id="herb-inner-search-box" type="text" placeholder="搜点什么吧..." style="height: 30px; width: 70%; border-radius: 15px; background-color: #d1d8e0; padding-left: 10px; padding-right: 10px; border: none; outline: none;"/>' +
-        '<button id="herb-search-btn" style="border: none; outline: none; background-color: white;">搜索</button>'
+        '<input id="herb-inner-search-box" type="text" placeholder="搜点什么吧..." style="margin-right: 5px; height: 30px; width: 75%; border-radius: 15px; background-color: #eeeeee; padding-left: 10px; padding-right: 10px; border: none; outline: none;"/>' +
+        '<button id="herb-search-btn" style="margin-left: 5px; border: none; outline: none; background-color: white;">搜索</button>'
     document.getElementsByTagName("body")[0].insertBefore(herbSearchBox, document.getElementById("content"));
     document.getElementById("herb-inner-search-box").value = decodeURIComponent(/^\?q=([\w%]+)/.exec(location.search)[1]);
     /* 搜索事件监听 */
